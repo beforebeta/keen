2c1cfa2d08009e4f0b39dc7e347dca425ee54038
Amrish Singh <beforebeta@gmail.com>
2013-09-25 16:31:17 -0400

    updates

--- keen/urls.py
+++ keen/urls.py
@@ -1,17 +1,21 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns
+from django.views.generic import TemplateView
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
+# urlpatterns = patterns('keen.views',
+#     url(r'^$', 'index'),
+#
+#     # Uncomment the admin/doc line below to enable admin documentation:
+#     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+#
+#     # Uncomment the next line to enable the admin:
+#     # url(r'^admin/', include(admin.site.urls)),
+# )
+
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'keen.views.home', name='home'),
-    # url(r'^keen/', include('keen.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    (r'^$', TemplateView.as_view(template_name='index.html')),
+    (r'^legal$', TemplateView.as_view(template_name='legal.html')),
 )
